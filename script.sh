df20fb4dfdc6ae3bd5ab25feec40cd0eb7f36e4f
arthursiq5 <arthur.siq5@gmail.com>
2022-07-25 20:52:34 -0300

    Criados grupos

--- script.sh
+++ script.sh
@@ -1,10 +1,17 @@
 #!/usr/bash
 
-echo "Criando diretórios"
+echo "Criando diretórios..."
 
 mkdir /publico
 mkdir /adm
 mkdir /ven
 mkdir /sec
 
-echo "Diretórios criados"
+echo "Diretórios criados!"
+echo "Criando grupos de usuários..."
+
+groupadd GRP_ADM
+groupadd GRP_VEN
+groupadd GRP_SEC
+
+echo "Grupos criados!"
