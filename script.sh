a6e46dd04a0f768eb3106362085c26cab10a0e61
arthursiq5 <arthur.siq5@gmail.com>
2022-07-25 20:55:08 -0300

    Criados usuários

--- script.sh
+++ script.sh
@@ -15,3 +15,16 @@ groupadd GRP_VEN
 groupadd GRP_SEC
 
 echo "Grupos criados!"
+echo "Criando usuários..."
+
+useradd carlos -m -s /bin/bash -p $(openssl passwd -crypt qwe123)
+useradd maria -m -s /bin/bash -p $(openssl passwd -crypt qwe123)
+useradd joao -m -s /bin/bash -p $(openssl passwd -crypt qwe123)
+useradd debora -m -s /bin/bash -p $(openssl passwd -crypt qwe123)
+useradd sebastiana -m -s /bin/bash -p $(openssl passwd -crypt qwe123)
+useradd roberto -m -s /bin/bash -p $(openssl passwd -crypt qwe123)
+useradd josefina -m -s /bin/bash -p $(openssl passwd -crypt qwe123)
+useradd amanda -m -s /bin/bash -p $(openssl passwd -crypt qwe123)
+useradd rogerio -m -s /bin/bash -p $(openssl passwd -crypt qwe123)
+
+echo "Usuários criados!"
